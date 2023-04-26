067324c05af62b37fe02697061f6024932147697
nicholasroether4308 <nicholas.roether@otto.de>
2023-04-26 15:08:07 +0200

    Implement event listener identifiers and removal

--- src/common/typed_events.ts
+++ src/common/typed_events.ts
@@ -2,23 +2,46 @@ type EventHandler<E> = (event: E) => unknown;
 
 abstract class EventEmitter<M> {
 	private readonly target: EventTarget;
+	private readonly identifierMap: Map<
+		number,
+		[type: string, listener: EventHandler<Event>]
+	>;
+	private nextIdentifier: number;
 
 	constructor() {
 		this.target = new EventTarget();
+		this.identifierMap = new Map();
+		this.nextIdentifier = 0;
 	}
 
 	public on<T extends keyof M & string>(
 		type: T,
 		handler: EventHandler<M[T]>
-	): void {
-		this.target.addEventListener(type, (evt) => {
+	): number {
+		const identifier = this.makeIdentifier();
+		const listener = (evt: Event): void => {
 			if (!("detail" in evt)) return;
 			handler(evt.detail as M[T]);
-		});
+		};
+		this.target.addEventListener(type, listener);
+		this.identifierMap.set(identifier, [type, listener]);
+		return identifier;
 	}
 
 	public emit<T extends keyof M & string>(type: T, event: M[T]): void {
 		this.target.dispatchEvent(new CustomEvent(type, { detail: event }));
+	}
+
+	public removeListener(identifier: number): void {
+		const entry = this.identifierMap.get(identifier);
+		if (!entry) return;
+		const [type, listener] = entry;
+		this.identifierMap.delete(identifier);
+		this.target.removeEventListener(type, listener);
+	}
+
+	private makeIdentifier(): number {
+		return this.nextIdentifier++;
 	}
 }
 
