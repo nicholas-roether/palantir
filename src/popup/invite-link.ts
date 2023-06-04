fee62558707565b2e8c6ff53c5b565bcf4260e5e
Nicholas Roether <nicholas.roether@t-online.de>
2023-06-05 00:52:34 +0200

    Fix invite link

--- src/popup/invite-link.ts
+++ src/popup/invite-link.ts
@@ -1,4 +1,4 @@
-const JOIN_SESSION_HOST = "https://palantir.niko-roether-dev/join/";
+const JOIN_SESSION_HOST = "https://palantir.niko-roether.dev/join/";
 
 function createInviteLink(username: string, hostId: string): string {
 	return JOIN_SESSION_HOST + btoa(`${username}:${hostId}`);
