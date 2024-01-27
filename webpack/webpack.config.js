8fcdec607123488ca7e8a1751513db2c0f864603
Nicholas Roether <nicholas.roether@t-online.de>
2024-01-27 19:22:33 +0100

    Create separate builds for firefox and chromium

--- webpack/webpack.config.js
+++ webpack/webpack.config.js
@@ -5,72 +5,82 @@ const unpluginIcons = require("unplugin-icons/webpack");
 const wd = path.resolve(__dirname, "..");
 const src = path.join(wd, "src");
 
-
-module.exports = (env) => ({
-	experiments: {
-		asyncWebAssembly: true
-	},
-	mode: env.production ? "production" : "development",
-	devtool: env.production ? undefined : "source-map",
-	entry: {
-		popup: path.join(src, "popup"),
-		join: path.join(src, "join"),
-		content: path.join(src, "content"),
-		background: path.join(src, "background"),
-		options: path.join(src, "options")
-	},
-	output: {
-		path: path.join(wd, "dist"),
-		clean: true
-	},
-	resolve: {
-		extensions: [".ts", ".js", ".tsx", ".jsx"]
-	},
-	module: {
-		rules: [
-			{
-				test: /\.tsx?$/,
-				loader: "babel-loader",
-				options: {
-					babelrc: false,
-					presets: [
-						"@babel/preset-env",
-						"@babel/preset-typescript",
-						"solid"
-					],
-					plugins: [
-						"@emotion"
-					]
+module.exports = function (env) {
+	const target = env.target ?? "firefox";
+	return {
+		experiments: {
+			asyncWebAssembly: true
+		},
+		mode: env.production ? "production" : "development",
+		devtool: env.production ? undefined : "source-map",
+		entry: {
+			popup: path.join(src, "popup"),
+			join: path.join(src, "join"),
+			content: path.join(src, "content"),
+			background: path.join(src, "background"),
+			options: path.join(src, "options")
+		},
+		output: {
+			path: path.join(wd, `dist/${target}`),
+			clean: true
+		},
+		resolve: {
+			extensions: [".ts", ".js", ".tsx", ".jsx"]
+		},
+		module: {
+			rules: [
+				{
+					test: /\.tsx?$/,
+					loader: "babel-loader",
+					options: {
+						babelrc: false,
+						presets: [
+							"@babel/preset-env",
+							"@babel/preset-typescript",
+							"solid"
+						],
+						plugins: ["@emotion"]
+					}
 				},
-			},
-			{
-				test: /\.css$/,
-				use: [
-					{
-						loader: "style-loader",
-						options: {
-							insert: (element) => {
-								if (window.__styleLoaderLastElement) {
-									window.__styleLoaderLastElement.after(element);
-									return;
+				{
+					test: /\.css$/,
+					use: [
+						{
+							loader: "style-loader",
+							options: {
+								insert: (element) => {
+									if (window.__styleLoaderLastElement) {
+										window.__styleLoaderLastElement.after(
+											element
+										);
+										return;
+									}
+									window.__styleLoaderLastElement = element;
+									document.head.prepend(element);
 								}
-								window.__styleLoaderLastElement = element;
-								document.head.prepend(element);
 							}
-						}
-					},
-					"css-loader"
-				]
-			}
-		]
-	},
-	plugins: [
-		unpluginIcons({ scale: 1.2, compiler: "solid", defaultStyle: "vertical-align: sub" }),
-		new CopyPlugin({
-			patterns: [
-				{ from: "./res", to: "." },
-				{ from: require.resolve("webextension-polyfill"), to: "."},
+						},
+						"css-loader"
+					]
+				}
 			]
-		})
-	]
-})
+		},
+		plugins: [
+			unpluginIcons({
+				scale: 1.2,
+				compiler: "solid",
+				defaultStyle: "vertical-align: sub"
+			}),
+			new CopyPlugin({
+				patterns: [
+					{ from: "./res", to: "." },
+					{ from: require.resolve("webextension-polyfill"), to: "." },
+					{
+						from: `manifest/${target}.json`,
+						to: "manifest.json"
+					}
+				]
+			})
+		]
+	};
+};
